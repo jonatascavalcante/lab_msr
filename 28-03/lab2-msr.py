f6fe35f23dde12090373082bcd1fd842470a3785
jonatascavalcante <jonatascavalcante777@gmail.com>
2019-04-07 17:14:53 -0300

    Fixing implementation issues

--- 28-03/lab2-msr.py
+++ 28-03/lab2-msr.py
@@ -1,40 +1,53 @@
 from git import Repo
-import time 
+import time
+import os, os.path
+from sets import Set
 
 CURRENT_YEAR = 2019
 ANALYZED_REPO = '/Users/jonatascavalcante/Projects/EventBus'
 
 # Functions definitions
 def count_qtd_files(commit):
-	stats = commit.stats
-	
-	return stats.total['files']
+	repo.git.checkout(commit)
+	qtd_files = sum([len(files) for root_folder, directory, files in os.walk(ANALYZED_REPO)])
+	return qtd_files
 
 
 def count_qtd_java_files(commit):
-	stats = commit.stats
+	repo.git.checkout(commit)
 	java_files = 0
-	
-	for file in stats.files:
-		if file.endswith('.java'):
-			java_files += 1
+
+	for root_folder, directory, files in os.walk(ANALYZED_REPO):
+		for file in files:
+			if file.endswith('.java'):
+				java_files += 1
 	
 	return java_files
 
 
 def count_qtd_java_lines(commit):
-	stats = commit.stats
+	repo.git.checkout(commit)
 	java_lines = 0
 	
-	for file, file_stats in stats.files.iteritems():
-		if file.endswith('.java'):
-			java_lines += file_stats['lines']
-	
+	for root_folder, directory, files in os.walk(ANALYZED_REPO):
+		for file in files:
+			if file.endswith('.java'):
+				file_name = root_folder + '/' + file
+				java_lines += file_len(file_name)
+				
 	return java_lines
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
 
 def get_all_commits_by_year_interval(all_commits, begin, end):
 	result_commits = []
+	
 	for commit in all_commits:
 		commit_year = time.gmtime(commit.committed_date).tm_year
 
@@ -44,45 +57,81 @@ def get_all_commits_by_year_interval(all_commits, begin, end):
 	return result_commits
 
 
+def qtd_files_by_year(commits_of_the_year):
+	files_of_year = Set([])
+	
+	for commit in commits_of_the_year:
+		repo.git.checkout(commit)
+		for root_folder, directory, files in os.walk(ANALYZED_REPO):
+			for file in files:
+				file_name = root_folder + '/' + file
+				files_of_year.add(file_name)
+
+	return len(files_of_year)
+
+
+def qtd_java_files_by_year(commits_of_the_year):
+	java_files_of_year = Set([])
+	
+	for commit in commits_of_the_year:
+		repo.git.checkout(commit)
+		for root_folder, directory, files in os.walk(ANALYZED_REPO):
+			for file in files:
+				if file.endswith('.java'):
+					file_name = root_folder + '/' + file
+					java_files_of_year.add(file_name)
+
+	return len(java_files_of_year)
+
+
+def qtd_java_lines_by_year(commits_of_the_year):
+	java_files_of_year = Set([])
+	qtd_java_lines = 0
+	
+	for commit in commits_of_the_year:
+		repo.git.checkout(commit)
+		for root_folder, directory, files in os.walk(ANALYZED_REPO):
+			for file in files:
+				if file.endswith('.java'):
+					file_name = root_folder + '/' + file
+					qtd_java_lines += file_len(file_name)
+
+	return qtd_java_lines
+
+
 def count_qtd_files_by_year(all_commits):
-	first_commit = all_commits[len(all_commits) - 1]
+	first_commit = all_commits[- 1]
 	first_commit_year = time.gmtime(first_commit.committed_date).tm_year
 	year = first_commit_year
 	
 	while year <= CURRENT_YEAR:
 		qtd_files = 0
-		commits_of_the_year = get_all_commits_by_year_interval(all_commits, year, year)
-		for commit in commits_of_the_year:
-			qtd_files += count_qtd_files(commit)
-		print str(year) + ": " + str(qtd_files) + " file(s)"
+		commits_of_the_year = get_all_commits_by_year_interval(all_commits, year, year)		
+		print str(year) + ": " + str(qtd_files_by_year(commits_of_the_year)) + " file(s)"
 		year += 1
 
 
 def count_qtd_java_files_by_year(all_commits):
-	first_commit = all_commits[len(all_commits) - 1]
+	first_commit = all_commits[- 1]
 	first_commit_year = time.gmtime(first_commit.committed_date).tm_year
 	year = first_commit_year
 	
 	while year <= CURRENT_YEAR:
 		qtd_java_files = 0
 		commits_of_the_year = get_all_commits_by_year_interval(all_commits, year, year)
-		for commit in commits_of_the_year:
-			qtd_java_files += count_qtd_java_files(commit)
-		print str(year) + ": " + str(qtd_java_files) + " java file(s)"
+		print str(year) + ": " + str(qtd_java_files_by_year(commits_of_the_year)) + " java file(s)"
 		year += 1
 
 
 def count_qtd_java_lines_by_year(all_commits):
-	first_commit = all_commits[len(all_commits) - 1]
+	first_commit = all_commits[- 1]
 	first_commit_year = time.gmtime(first_commit.committed_date).tm_year
 	year = first_commit_year
 	
 	while year <= CURRENT_YEAR:
 		qtd_java_lines = 0
 		commits_of_the_year = get_all_commits_by_year_interval(all_commits, year, year)
-		for commit in commits_of_the_year:
-			qtd_java_lines += count_qtd_java_lines(commit)
-		print str(year) + ": " + str(qtd_java_lines) + " java code line(s)"
+		print str(year) + ": " + str(qtd_java_lines_by_year(commits_of_the_year)) + " java code line(s)"
 		year += 1
 
 # End of functions definitions
@@ -91,17 +140,18 @@ def count_qtd_java_lines_by_year(all_commits):
 repo = Repo(ANALYZED_REPO)
 assert not repo.bare
 all_commits = list(repo.iter_commits('master'))
-first_commit_index = len(all_commits) - 1
+
+repo.git.checkout(all_commits[0])
 
 # Question 1
 print "Question 1:"
 print "Files in the last commit: " + str(count_qtd_files(all_commits[0]))
-print "Files in the first commit: " + str(count_qtd_files(all_commits[first_commit_index]))
+print "Files in the first commit: " + str(count_qtd_files(all_commits[-1]))
 
 # Question 2
 print "\nQuestion 2:"
 print "Java files in the last commit: " + str(count_qtd_java_files(all_commits[0]))
-print "Java files in the first commit: " + str(count_qtd_java_files(all_commits[first_commit_index]))
+print "Java files in the first commit: " + str(count_qtd_java_files(all_commits[-1]))
 
 # Question 3
 print "\nQuestion 3:"
@@ -118,14 +168,14 @@ print "\nQuestion 5:"
 for commit in all_commits:
 	print commit.hexsha + ": " + str(count_qtd_java_lines(commit)) + " java code line(s)"
 
-#Question 6
+# Question 6
 print "\nQuestion 6:"
 count_qtd_files_by_year(all_commits)
 
-#Question 7
+# Question 7
 print "\nQuestion 7:"
 count_qtd_java_files_by_year(all_commits)
 
-#Question 8
+# Question 8
 print "\nQuestion 8:"
 count_qtd_java_lines_by_year(all_commits)
