02fc63ea87c081ac9dccd82458750ddb11a07bfb
samoliveira <samuelmoliveira0@gmail.com>
2019-04-09 17:10:49 -0300

    list 3, excercises 2,4

--- 3. 04-04/lab3-msr.py
+++ 3. 04-04/lab3-msr.py
@@ -1,7 +1,14 @@
 from git import Repo
 import os, os.path
+import platform
+import time
+import datetime
+from datetime import date, timedelta
 
-ANALYZED_REPO = '/Users/jonatascavalcante/Projects/EventBus'
+if platform.system() == "Windows":
+	ANALYZED_REPO = 'C:\\Users\\samue\\EventBus'
+else:
+	ANALYZED_REPO = '/Users/jonatascavalcante/Projects/EventBus'
 
 # Functions definitions
 def cta_mca(fname):
@@ -93,12 +100,20 @@ def biggest_cta_cra_mca(commit):
 
 	biggest_cta_cra_mca = {
 		"big_cta": big_cta_fname,
+		"big_cta_val": big_cta_value,
 		"big_cra": big_cra_fname,
-		"big_mca": big_mca_fname
+		"big_cra_val": big_cra_value,
+		"big_mca": big_mca_fname,
+		"big_mca_val": big_mca_value
 	}
 
 	return biggest_cta_cra_mca
 
+
+def get_all_commits_interval(repo, since, before):
+	a = list(repo.iter_commits(since=since, before=before))
+	
+	return a
 
 # End of functions definitions
 
@@ -109,23 +124,36 @@ all_commits = list(repo.iter_commits('master'))
 
 repo.git.checkout(all_commits[0])
 
+maxdt = date.today() - timedelta(365)
+mindt = date.today() - timedelta(10*365)
+
+
 # Question 1
 print "Question 1:"
-biggest_cta_cra_mca = biggest_cta_cra_mca(all_commits[0])
-print "Biggest CTA: " + str(biggest_cta_cra_mca["big_cta"])
-print "Biggest CRA: " + str(biggest_cta_cra_mca["big_cra"])
-print "Biggest MCA: " + str(biggest_cta_cra_mca["big_mca"])
+biggest_dict = biggest_cta_cra_mca(all_commits[0])
+print "Biggest CTA: " + str(biggest_dict["big_cta"]) +":"+ str(biggest_dict["big_cta_val"]) 
+print "Biggest CRA: " + str(biggest_dict["big_cra"]) +":"+ str(biggest_dict["big_cra_val"]) 
+print "Biggest MCA: " + str(biggest_dict["big_mca"]) +":"+ str(biggest_dict["big_mca_val"]) 
 
 # # Question 2
-# print "\nQuestion 2:"
+print "\nQuestion 2:"
+ly_commits = get_all_commits_interval(repo=Repo(ANALYZED_REPO), since=mindt, before=maxdt)
+
+biggest_cta_cra_mca2 = biggest_cta_cra_mca(ly_commits[0])
+
+print "Biggest CTA: " + str(biggest_cta_cra_mca2["big_cta"]) +":"+ str(biggest_cta_cra_mca2["big_cta_val"]) 
+print "Biggest CRA: " + str(biggest_cta_cra_mca2["big_cra"]) +":"+ str(biggest_cta_cra_mca2["big_cra_val"]) 
+print "Biggest MCA: " + str(biggest_cta_cra_mca2["big_mca"]) +":"+ str(biggest_cta_cra_mca2["big_mca_val"]) 
+
+
 
 # Question 3
 print "\nQuestion 3:"
 print "CP of the last commit: " + str(cp(all_commits[0]))
 
 # # Question 4
-# print "\nQuestion 4:"
-# print "CP of the last commit of one year ago: " + str(cp(all_commits[0]))
+print "\nQuestion 4:"
+print "CP of the last commit of one year ago: " + str(cp(ly_commits[0]))
 
 # Question 5
 print "\nQuestion 5:"
