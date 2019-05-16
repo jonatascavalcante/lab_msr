2c06d57e5751686a0c2d5d1b330e3be227611553
jonatascavalcante <jonatascavalcante777@gmail.com>
2019-05-16 20:31:06 -0300

    Full lab 8 implementation

--- 8. 16-05/lab8-msr.py
+++ 8. 16-05/lab8-msr.py
@@ -1,24 +1,77 @@
 from github import Github
 
-g = Github("a7ea3343e2e00529d1a27e4a00a93ca61d509fd3")
+TOKEN_SAMUEL = "a7ea3343e2e00529d1a27e4a00a93ca61d509fd3"
+TOKEN_JONATAS = "a7575b8feaa470e3116995c2e50b2c8e2c12d46d"
 
-"""
-for repo in g.get_user().get_repos():
-    print(repo.name)
-"""
-print("1 - ")
+g = Github(TOKEN_JONATAS)
+
+print("\nQuestao 1")
 print(g.get_repo("torvalds/linux").get_languages())
 
-
-print("2 - ")
+print("\nQuestao 2")
 for tag in g.get_repo("torvalds/linux").get_tags()[:10]:
     print(tag.name)
 
-print("3 - ")
+print("\nQuestao 3")
 for repo in g.get_user("torvalds").get_repos():
     print(repo.name)
 
-print("4 - ")
+print("\nQuestao 4")
 query = "language:c"
-for repo in g.search_repositories(query, sort="stars", order="desc"):
+for repo in g.search_repositories(query, sort="stars", order="desc")[:10]:
     print(repo.full_name)
+
+print("\nQuestao 5")
+query = "language:c"
+for repo in g.search_repositories(query, sort="forks", order="desc")[:10]:
+    print(repo.full_name)
+
+print("\nQuestao 6")
+query = "topic:microservices"
+for repo in g.search_repositories(query, sort="stars", order="desc")[:10]:
+    print(repo.full_name)
+
+print("\nQuestao 7")
+query = "language:javascript topic:html5"
+for repo in g.search_repositories(query, sort="stars", order="desc")[:10]:
+    print(repo.full_name)
+
+print("\nQuestao 8")
+query = "fakenews"
+for repo in g.search_repositories(query, sort="stars", order="desc")[:10]:
+    print(repo.full_name)
+
+print("\nQuestao 9")
+query = "android in:name"
+for repo in g.search_repositories(query)[:10]:
+    print(repo.full_name)
+
+print("\nQuestao 10")
+query = "android client in:description"
+for repo in g.search_repositories(query)[:10]:
+    print(repo.full_name)
+
+print("\nQuestao 11")
+query = "circleCI in:readme"
+for repo in g.search_repositories(query)[:10]:
+    print(repo.full_name) 
+
+print("\nQuestao 12")
+query = "label:critical language:javascript"
+for issue in g.search_issues(query)[:10]:
+    print(issue.title) 
+
+print("\nQuestao 13")
+query = "label:bug language:Go state:closed"
+for issue in g.search_issues(query)[:10]:
+    print(issue.title) 
+
+print("\nQuestao 14")
+query = "label:needhelp language:javascript state:open"
+for issue in g.search_issues(query)[:10]:
+    print(issue.title)
+
+print("\nQuestao 15")
+query = "label:feature state:open"
+for issue in g.search_issues(query, sort="created", order="asc")[:10]:
+    print(issue.title)
